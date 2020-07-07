d99f9c02bce5277d4485bce002f1e099715eff78
Muhammad-Ahmed-Mansoor <67926442+Muhammad-Ahmed-Mansoor@users.noreply.github.com>
2020-07-07 22:09:51 +0500

    Add files via upload

---
+++ Tables/TimerResources/removespaces.py
@@ -0,0 +1,10 @@
+# first get all lines from file
+with open('file.txt', 'r') as f:
+    lines = f.readlines()
+
+# remove spaces
+lines = [line.replace('\n', '') for line in lines]
+
+# finally, write lines in the file
+with open('file.txt', 'w') as f:
+    f.writelines(lines)
